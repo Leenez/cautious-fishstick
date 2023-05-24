5846975c00557a0a1a8d52828895f420b1a2c4a1
unknown <jani_leinonen@hotmail.com>
2023-05-24 09:25:36 +0300

    dbConnect

--- server.js
+++ server.js
@@ -14,18 +14,10 @@ const mongo_password = process.env.MONGODB_PASSWORD;
 const database = "contactdatabase";
 const url = `mongodb+srv://${mongo_user}:${mongo_password}@${mongo_url}/${database}?retryWrites=true&w=majority`
 
-//mongoose.connect(url).then(
-//    () => console.log("Connected to MongoDB"),
-//    (error) => console.log("Failed",error)
-//)
-
 const dbConnect = async (url) => {
-    const connection = await mongoose.connect(url);
-    if (connection) { 
-        console.log("Connected to MongoDB")
-    } else {
-        console.log("Not connected")
-    }
+    await mongoose.connect(url).then(
+        () => console.log("Connected to MongoDB"),
+        (error) => console.log("Failed",error))
 }
 
 dbConnect(url);
